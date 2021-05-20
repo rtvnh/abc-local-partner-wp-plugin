0e5ad56d7f7d58d4a5e8c4d093c7aeacce60133e
Elmar Wouters <elmar@angrybytes.com>
2021-05-20 16:00:46 +0200

    Basic check for updates settings

--- updater.php
+++ updater.php
@@ -107,4 +107,71 @@ class AbcLocalPartnerWp_Updater
         return $transient;
     }
 
+    public function plugin_popup($result, $action, $args)
+    {
+
+        if (!empty($args->slug)) {
+
+            if ($args->slug == current(explode('/', $this->basename))) {
+
+                $this->get_repository_info();
+
+                $plugin = array(
+                    'name' => $this->plugin["Name"],
+                    'slug' => $this->basename,
+                    'requires' => '3.3',
+                    'tested' => '4.4.1',
+                    'rating' => '100.0',
+                    'num_ratings' => '10823',
+                    'downloaded' => '14249',
+                    'added' => '2016-01-05',
+                    'version' => $this->github_response['tag_name'],
+                    'author' => $this->plugin["AuthorName"],
+                    'author_profile' => $this->plugin["AuthorURI"],
+                    'last_updated' => $this->github_response['published_at'],
+                    'homepage' => $this->plugin["PluginURI"],
+                    'short_description' => $this->plugin["Description"],
+                    'sections' => array(
+                        'Description' => $this->plugin["Description"],
+                        'Updates' => $this->github_response['body'],
+                    ),
+                    'download_link' => $this->github_response['zipball_url']
+                );
+
+                return (object)$plugin;
+            }
+
+        }
+        return $result;
+    }
+
+    public function download_package($args, $url)
+    {
+
+        if (null !== $args['filename']) {
+            if ($this->authorize_token) {
+                $args = array_merge($args, array("headers" => array("Authorization" => "token {$this->authorize_token}")));
+            }
+        }
+
+        remove_filter('http_request_args', [$this, 'download_package']);
+
+        return $args;
+    }
+
+    public function after_install($response, $hook_extra, $result)
+    {
+        global $wp_filesystem;
+
+        $install_directory = plugin_dir_path($this->file);
+        $wp_filesystem->move($result['destination'], $install_directory);
+        $result['destination'] = $install_directory;
+
+        if ($this->active) {
+            activate_plugin($this->basename);
+        }
+
+        return $result;
+    }
+
 }
