8f22e669dc73f40b722f4ba2fb33e96d357ba5b3
Elmar Wouters <elmar@angrybytes.com>
2021-05-20 16:19:48 +0200

    Added renewed settings

--- abc-local-partner-wp-plugin.php
+++ abc-local-partner-wp-plugin.php
@@ -35,55 +35,35 @@ $updater->authorize( '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' );
 
 $updater->initialize();
 
-function abclocalpartner_add_settings_page()
-{
-    add_options_page('ABC Manager Page', 'ABC Manager Plugin Menu', 'manage_options', 'abclocalpartner-example-plugin', 'abclocalpartner_render_plugin_settings_page');
+function abclocalpartner_register_settings() {
+    add_option( 'abclocalpartner_option_name', 'This is my option value.');
+    register_setting( 'abclocalpartner_options_group', 'abclocalpartner_option_name', 'abclocalpartner_callback' );
 }
+add_action( 'admin_init', 'abclocalpartner_register_settings' );
 
-add_action('admin_menu', 'abclocalpartner_add_settings_page');
+function abclocalpartner_register_options_page() {
+    add_options_page('Page Title', 'Plugin Menu', 'manage_options', 'abclocalpartner', 'abclocalpartner_options_page');
+}
+add_action('admin_menu', 'abclocalpartner_register_options_page');
 
-function abclocalpartner_render_plugin_settings_page()
+function abclocalpartner_option_page()
 {
     ?>
-    <h2>ABC Manager Plugin Settings</h2>
-    <form action="options.php" method="post">
-        <?php
-        settings_fields('abclocalpartner_example_plugin_options');
-        do_settings_sections('abclocalpartner_example_plugin'); ?>
-        <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e('Save'); ?>"/>
-    </form>
+    <div>
+        <?php screen_icon(); ?>
+        <h2>ABC Manager Page Title</h2>
+        <form method="post" action="options.php">
+            <?php settings_fields( 'abclocalpartner_options_group' ); ?>
+            <h3>ABC Manager Options</h3>
+            <p>Please adjust the settings, for connection with ABC</p>
+            <table>
+                <tr valign="top">
+                    <th scope="row"><label for="abclocalpartner_option_name">ABC Manager URL</label></th>
+                    <td><input type="text" id="abclocalpartner_option_name" name="abclocalpartner_option_name" value="<?php echo get_option('abclocalpartner_option_name'); ?>" /></td>
+                </tr>
+            </table>
+            <?php  submit_button(); ?>
+        </form>
+    </div>
     <?php
-}
-
-function abclocalpartner_example_plugin_options_validate($input)
-{
-    $newinput['api_key'] = trim($input['api_key']);
-    if (!preg_match('/^[a-z0-9]{32}$/i', $newinput['api_key'])) {
-        $newinput['api_key'] = '';
-    }
-
-    return $newinput;
-}
-
-function abclocalpartner_plugin_section_text()
-{
-    echo '<p>Here you can set all the options for using the API</p>';
-}
-
-function abclocalpartner_plugin_setting_api_key()
-{
-    $options = get_option('abclocalpartner_example_plugin_options');
-    echo "<input id='abclocalpartner_plugin_setting_api_key' name='abclocalpartner_example_plugin_options[api_key]' type='text' value='" . esc_attr($options['api_key']) . "' />";
-}
-
-function abclocalpartner_plugin_setting_results_limit()
-{
-    $options = get_option('abclocalpartner_example_plugin_options');
-    echo "<input id='abclocalpartner_plugin_setting_results_limit' name='abclocalpartner_example_plugin_options[results_limit]' type='text' value='" . esc_attr($options['results_limit']) . "' />";
-}
-
-function abclocalpartner_plugin_setting_start_date()
-{
-    $options = get_option('abclocalpartner_example_plugin_options');
-    echo "<input id='abclocalpartner_plugin_setting_start_date' name='abclocalpartner_example_plugin_options[start_date]' type='text' value='" . esc_attr($options['start_date']) . "' />";
 }
