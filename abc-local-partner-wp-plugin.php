5cefefca5726b83582488844d4d49e2831c219e6
Elmar Wouters <elmar@angrybytes.com>
2021-05-21 09:21:43 +0200

    Settings update, settings are now stored as abclocalpartner_option_etc

--- abc-local-partner-wp-plugin.php
+++ abc-local-partner-wp-plugin.php
@@ -12,7 +12,7 @@ Plugin URI: https://abcmanager.nl/
 
 Description: Wordpress Plugin to post new updates to ABC Manager of NH/AT5
 
-Version: 0.1.2
+Version: 0.1.3
 
 Author: AngryBytes B.V.
 
@@ -24,45 +24,76 @@ Text Domain: abclocalpartner
 
 */
 
-if( ! class_exists( 'AbcLocalPartnerWp_Updater' ) ){
-    include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+if (!class_exists('AbcLocalPartnerWp_Updater')) {
+    include_once(plugin_dir_path(__FILE__) . 'updater.php');
 }
 
-$updater = new AbcLocalPartnerWp_Updater( __FILE__ );
+$updater = new AbcLocalPartnerWp_Updater(__FILE__);
 $updater->set_username('rtvnh');
 $updater->set_repository('abc-local-partner-wp-plugin');
-$updater->authorize( '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' );
+$updater->authorize('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');
 
 $updater->initialize();
 
-function abclocalpartner_register_settings() {
-    add_option( 'abclocalpartner_option_name', 'This is my option value.');
-    register_setting( 'abclocalpartner_options_group', 'abclocalpartner_option_name', 'abclocalpartner_callback' );
+function abclocalpartner_register_settings()
+{
+    add_option('abclocalpartner_option_abc_url', 'Fill in your domain here');
+    add_option('abclocalpartner_option_partner_secret', 'Your partner secret');
+    register_setting(
+        'abclocalpartner_options_group',
+        'abclocalpartner_option_abc_url',
+        'abclocalpartner_option_partner_secret',
+        'abclocalpartner_callback'
+    );
 }
-add_action( 'admin_init', 'abclocalpartner_register_settings' );
 
-function abclocalpartner_register_options_page() {
-    add_options_page('Page Title', 'Plugin Menu', 'manage_options', 'abclocalpartner', 'abclocalpartner_options_page');
+add_action('admin_init', 'abclocalpartner_register_settings');
+
+function abclocalpartner_register_options_page()
+{
+    add_options_page('ABC Manager - Connect', 'ABC Manager', 'manage_options', 'abclocalpartner', 'abclocalpartner_options_page');
 }
+
 add_action('admin_menu', 'abclocalpartner_register_options_page');
 
-function abclocalpartner_option_page()
+function abclocalpartner_options_page()
 {
     ?>
     <div>
-        <?php screen_icon(); ?>
-        <h2>ABC Manager Page Title</h2>
+        <h1>RTV NH/AT5 - ABC Manager</h1>
+        <div>
+            <img src="<?php echo plugin_dir_url(__FILE__) ?>/assets/images/nh-at5-logo.png" height="64" width="64"
+                 alt="NH"/>
+        </div>
         <form method="post" action="options.php">
-            <?php settings_fields( 'abclocalpartner_options_group' ); ?>
-            <h3>ABC Manager Options</h3>
+            <?php settings_fields('abclocalpartner_options_group'); ?>
+            <h2>ABC Manager Options</h2>
             <p>Please adjust the settings, for connection with ABC</p>
-            <table>
-                <tr valign="top">
-                    <th scope="row"><label for="abclocalpartner_option_name">ABC Manager URL</label></th>
-                    <td><input type="text" id="abclocalpartner_option_name" name="abclocalpartner_option_name" value="<?php echo get_option('abclocalpartner_option_name'); ?>" /></td>
+            <table class="form-table" role="presentation">
+                <tbody>
+                <tr>
+                    <th scope="row">
+                        <label for="abclocalpartner_option_abc_url">ABC Manager URL API</label>
+                    </th>
+                    <td>
+                        <input type="text" id="abclocalpartner_option_abc_url" name="abclocalpartner_option_abc_url"
+                               class="regular-text"
+                               value="<?php echo get_option('abclocalpartner_option_abc_url'); ?>"/>
+                    </td>
                 </tr>
+                <tr>
+                    <th scope="row">
+                        <label for="abclocalpartner_option_partner_secret">Partner Token</label>
+                    </th>
+                    <td>
+                        <input type="text" id="abclocalpartner_option_partner_secret"
+                               name="abclocalpartner_option_partner_secret" class="regular-text"
+                               value="<?php echo get_option('abclocalpartner_option_partner_secret'); ?>"/>
+                    </td>
+                </tr>
+                </tbody>
             </table>
-            <?php  submit_button(); ?>
+            <?php submit_button(); ?>
         </form>
     </div>
     <?php
