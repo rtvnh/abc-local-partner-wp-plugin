7c0a6442c4a4d3ae63c8759fe6d918f60993723d
t0mmie <thomas@angrybytes.com>
2021-08-17 17:06:38 +0200

    Code cleanup

--- abc-local-partner-wp-plugin.php
+++ abc-local-partner-wp-plugin.php
@@ -3,21 +3,19 @@
 /**
  * @package AbcLocalPartnerWp
  *
- * Plugin Name:        ABC Manager - Local Partner Wordpress
- * Plugin URI:         https://abcmanager.nl/
- * Description:        Wordpress Plugin to post new updates to ABC Manager of NH/AT5
- * Version:            0.7.1
- * Author:             AngryBytes B.V.
- * Author URI:         https://angrybytes.com
- * License:            MIT
- * Text Domain:        abclocalpartner
- * Requires at least:  5.7
- * Requires PHP:       7.3
+ * Plugin Name			ABC Manager - Local Partner
+ * Plugin URI:			https://github.com/rtvnh/abc-local-partner-wp-plugin
+ * Description:			WordPress Plugin to post new updates to the ABC Manager of NH/AT5
+ * Version:				0.8.1
+ * Author:				AngryBytes B.V.
+ * Author URI:			https://angrybytes.com
+ * License:				MIT
+ * Text Domain:			abclocalpartner
+ * Requires at least:	5.7
+ * Requires PHP:		7.3
  */
 
-/*
-* Updater
-*/
+// Configure our plugin updater.
 include_once plugin_dir_path( __FILE__ ) . '/updater.php';
 
 $updater = new AbcLocalPartnerWp_Updater( __FILE__ );
@@ -25,8 +23,8 @@ $updater->set_username( 'rtvnh' );
 $updater->set_repository( 'abc-local-partner-wp-plugin' );
 $updater->initialize();
 
-/*
- * Registering of settings
+/**
+ * Register our plugin settings.
  */
 function abclocalpartner_register_settings() {
 	add_option( 'abclocalpartner_option_abc_url' );
@@ -63,8 +61,8 @@ function abclocalpartner_register_settings() {
 
 add_action( 'admin_init', 'abclocalpartner_register_settings' );
 
-/*
- * Registers the options page for ABC Manager plugin
+/**
+ * Register the options page for our plugin.
  */
 function abclocalpartner_register_options_page() {
 	add_options_page( 'ABC Manager - Connect', 'ABC Manager', 'manage_options', 'abclocalpartner', 'abclocalpartner_options_page' );
@@ -72,122 +70,125 @@ function abclocalpartner_register_options_page() {
 
 add_action( 'admin_menu', 'abclocalpartner_register_options_page' );
 
-/*
- * Generates the options page for ABC Manager plugin
+/**
+ * Generate the options page for our plugin.
  */
 function abclocalpartner_options_page() {
 	?>
-    <div>
-        <h1>RTV NH/AT5 - ABC Manager</h1>
-        <div>
-            <img src="<?php echo plugin_dir_url( __FILE__ ) ?>/assets/images/nh-at5-logo.png" height="64" width="64"
-                 alt="NH"/>
-        </div>
-        <form method="post" action="options.php">
+	<div>
+		<h1>RTV NH/AT5 - ABC Manager</h1>
+		<div>
+			<img src="<?php echo plugin_dir_url( __FILE__ ) ?>/assets/images/nh-at5-logo.png" height="64" width="64"
+				 alt="NH"/>
+		</div>
+		<form method="post" action="options.php">
 			<?php settings_fields( 'abclocalpartner_options_group' ); ?>
-            <h2>ABC Manager Options</h2>
-            <p>Please adjust the settings, for connection with ABC. If your not sure to know which codes are required,
-               please read the docs.</p>
-            <table class="form-table" role="presentation">
-                <tbody>
-                <tr>
-                    <th scope="row">
-                        <label for="abclocalpartner_option_abc_url">ABC Manager URL API</label>
-                    </th>
-                    <td>
-                        <input type="text" id="abclocalpartner_option_abc_url" name="abclocalpartner_option_abc_url"
-                               class="regular-text"
-                               value="<?php echo get_option( 'abclocalpartner_option_abc_url' ); ?>"/>
-                    </td>
-                </tr>
+			<h2>ABC Manager Options</h2>
+			<p>Please adjust the settings, for connection with ABC. If your not sure to know which codes are required,
+			   please read the docs.</p>
+			<table class="form-table" role="presentation">
+				<tbody>
 				<tr>
-                    <th scope="row">
-                        <label for="abclocalpartner_option_partner_name">Partner Name On ABC Manager</label>
-                    </th>
-                    <td>
-                        <input type="text" id="abclocalpartner_option_partner_name"
-                               name="abclocalpartner_option_partner_name" class="regular-text"
-                               value="<?php echo get_option( 'abclocalpartner_option_partner_name' ); ?>"/>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">
-                        <label for="abclocalpartner_option_partner_client_id">Partner Client ID</label>
-                    </th>
-                    <td>
-                        <input type="text" id="abclocalpartner_option_partner_client_id"
-                               name="abclocalpartner_option_partner_client_id" class="regular-text"
-                               value="<?php echo get_option( 'abclocalpartner_option_partner_client_id' ); ?>"/>
-                    </td>
-                </tr>
+					<th scope="row">
+						<label for="abclocalpartner_option_abc_url">ABC Manager URL API</label>
+					</th>
+					<td>
+						<input type="text" id="abclocalpartner_option_abc_url" name="abclocalpartner_option_abc_url"
+							   class="regular-text"
+							   value="<?php echo get_option( 'abclocalpartner_option_abc_url' ); ?>"/>
+					</td>
+				</tr>
 				<tr>
-                    <th scope="row">
-                        <label for="abclocalpartner_option_partner_client_secret">Partner Client Secret</label>
-                    </th>
-                    <td>
-                        <input type="text" id="abclocalpartner_option_partner_client_secret"
-                               name="abclocalpartner_option_partner_client_secret" class="regular-text"
-                               value="<?php echo get_option( 'abclocalpartner_option_partner_client_secret' ); ?>"/>
-                    </td>
-                </tr>
-                <style type="text/css">
-                    .status-dot {
-                        height: 10px;
-                        width: 10px;
-                        border-radius: 1000px;
-                        display: inline-block;
-                    }
+					<th scope="row">
+						<label for="abclocalpartner_option_partner_name">Partner Name On ABC Manager</label>
+					</th>
+					<td>
+						<input type="text" id="abclocalpartner_option_partner_name"
+							   name="abclocalpartner_option_partner_name" class="regular-text"
+							   value="<?php echo get_option( 'abclocalpartner_option_partner_name' ); ?>"/>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="abclocalpartner_option_partner_client_id">Partner Client ID</label>
+					</th>
+					<td>
+						<input type="text" id="abclocalpartner_option_partner_client_id"
+							   name="abclocalpartner_option_partner_client_id" class="regular-text"
+							   value="<?php echo get_option( 'abclocalpartner_option_partner_client_id' ); ?>"/>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="abclocalpartner_option_partner_client_secret">Partner Client Secret</label>
+					</th>
+					<td>
+						<input type="text" id="abclocalpartner_option_partner_client_secret"
+							   name="abclocalpartner_option_partner_client_secret" class="regular-text"
+							   value="<?php echo get_option( 'abclocalpartner_option_partner_client_secret' ); ?>"/>
+					</td>
+				</tr>
+				<style type="text/css">
+					.status-dot {
+						height: 10px;
+						width: 10px;
+						border-radius: 1000px;
+						display: inline-block;
+					}
 
-                    .status-dot.is-red {
-                        background-color: red;
-                    }
+					.status-dot.is-red {
+						background-color: red;
+					}
 
-                    .status-dot.is-green {
-                        background-color: green;
-                    }
-                </style>
+					.status-dot.is-green {
+						background-color: green;
+					}
+				</style>
 				<?php
-				// Are ABC and Wordpress connected?
+				// Are ABC Manager and WordPress connected?
 				$headers = @get_headers( get_option( 'abclocalpartner_option_abc_url' ) );
+				// TODO should attempt to authenticate with ABC Manager
 
 				if ( $headers === false ) {
-					$status      = 'down';
+					$status	  = 'down';
 					$statusColor = 'red';
 				} else if ( $headers && strpos( $headers[0], '301' ) ) {
-					$status      = 'down';
+					$status	  = 'down';
 					$statusColor = 'red';
 				} else {
-					$status      = 'up';
+					$status	  = 'up';
 					$statusColor = 'green';
 				}
 				?>
-                <tr>
-                    <th>
-                        Current connection state:
-                    </th>
-                    <td>
+				<tr>
+					<th>
+						Current connection state:
+					</th>
+					<td>
 						<?= $status ?> <span class="status-dot is-<?= $statusColor ?>"></span>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+					</td>
+				</tr>
+				</tbody>
+			</table>
 			<?php submit_button(); ?>
-        </form>
-    </div>
+		</form>
+	</div>
 	<?php
 }
 
-/*
- * Get bearer token
+/**
+ * Get a bearer token from ABC Manager.
+ *
+ * @return mixed
  */
-function get_abc_bearer_token($apiEndpoint, $clientId, $clientSecret) {
-	$raw_response = wp_remote_post($apiEndpoint . '/oauth2/token', [
+function get_abc_bearer_token(string $api_endpoint, string $client_ID, string $client_secret) {
+	$raw_response = wp_remote_post($api_endpoint . '/oauth2/token', [
 		'method' => 'POST',
 		'headers' => array('Content-Type' => 'application/x-www-form-urlencoded'),
 		'body' => array(
 			'grant_type' => 'client_credentials',
-			'client_id' => $clientId,
-			'client_secret' => $clientSecret,
+			'client_id' => $client_ID,
+			'client_secret' => $client_secret,
 			'scope' => 'partners'
 		)
 	]);
@@ -196,21 +197,30 @@ function get_abc_bearer_token($apiEndpoint, $clientId, $clientSecret) {
 	return $response['access_token'];
 }
 
-/*
- * POST Article to ABC Manager
+/**
+ * Post an article to ABC Manager.
+ *
+ * @return bool
  */
-function post_article_to_abc_manager($post, $postGalleries, $apiEndpoint, $bearerToken, $partnerName, $isRetry) {
+function post_article_to_abc_manager(
+        WP_Post $post,
+        array $post_galleries,
+        string $api_endpoint,
+        string $bearer_token,
+        string $partner_name,
+        bool $is_retry
+) {
 	$postJson = wp_json_encode($post);
-	$galleriesJson = wp_json_encode($postGalleries);
+	$galleriesJson = wp_json_encode($post_galleries);
 
-	$response = wp_remote_post($apiEndpoint . '/partner/article', [
-		'body'    => [
-			'partner' => $partnerName,
+	$response = wp_remote_post($api_endpoint . '/partner/article', [
+		'body'	=> [
+			'partner' => $partner_name,
 			'content' => $postJson,
 			'galleries' => $galleriesJson,
 		],
 		'headers' => [
-			'Authorization' => 'Bearer ' . $bearerToken,
+			'Authorization' => 'Bearer ' . $bearer_token,
 		]
 	]);
 
@@ -219,7 +229,7 @@ function post_article_to_abc_manager($post, $postGalleries, $apiEndpoint, $beare
 		return true;
 	}
 	if ($response['body'] === 'Unauthorized') {
-		if($isRetry) {
+		if($is_retry) {
 			// TODO add an error message: An error has occurred sending the post to ABC Manager, Make sure the credentials is correct.
 			return true;
 		}
@@ -229,10 +239,10 @@ function post_article_to_abc_manager($post, $postGalleries, $apiEndpoint, $beare
 	return true;
 }
 
-/*
- * Performs a POST request to ABC Manager
+/**
+ * Register a hook on "save_post".
  */
-function abclocalpartner_post_to_abc($postid) {
+function abclocalpartner_post_to_abc(int $post_ID) { // TODO accept all params from save_post hook (int $post_ID, WP_Post $post, bool $update)
 	if (!empty(get_option('abclocalpartner_option_abc_url')) &&
 		!empty(get_option('abclocalpartner_option_partner_client_id')) &&
 		!empty(get_option('abclocalpartner_option_partner_client_secret'))
@@ -243,9 +253,9 @@ function abclocalpartner_post_to_abc($postid) {
 		$clientSecret = get_option('abclocalpartner_option_partner_client_secret');
 		$bearerToken= get_option('abclocalpartner_option_access_token');
 
-		if (get_post_status($postid) == 'publish' ) {
-			$post = get_post($postid);
-			$postGalleries = get_post_galleries($postid);
+		if (get_post_status($post_ID) == 'publish' ) {
+			$post = get_post($post_ID);
+			$postGalleries = get_post_galleries($post_ID);
 
 			if (empty($bearerToken)) {
 				$bearerToken = get_abc_bearer_token($apiEndpoint, $clientId, $clientSecret);
@@ -258,23 +268,28 @@ function abclocalpartner_post_to_abc($postid) {
 				$bearerToken = get_abc_bearer_token($apiEndpoint, $clientId, $clientSecret);
 				update_option('abclocalpartner_option_access_token', $bearerToken);
 				post_article_to_abc_manager($post, $postGalleries, $apiEndpoint, $bearerToken, $partnerName, true);
-			};
+			}
 		}
 	}
 }
 
 add_action('save_post', 'abclocalpartner_post_to_abc');
 
-function prefix_add_source_tag($tags, $context) {
-    if ('post' === $context) {
-        $tags['iframe'] = array(
-            'src'    => true,
-            'srcdoc' => true,
-            'width'  => true,
-            'height' => true,
-        );
-    }
-    return $tags;
+/**
+ * Allow iframe HTML tags.
+ *
+ * @return array
+ */
+function prefix_add_source_tag(array $tags, string $context) {
+	if ('post' === $context) {
+		$tags['iframe'] = array(
+			'src'	=> true,
+			'srcdoc' => true,
+			'width'  => true,
+			'height' => true,
+		);
+	}
+	return $tags;
 }
 
 add_filter('wp_kses_allowed_html', 'prefix_add_source_tag', 10, 2);
--- updater.php
+++ updater.php
@@ -1,168 +1,232 @@
 <?php
 
+/**
+ * Updater class to deploy the WordPress ABC Local Partner plugin using GitHub.
+ */
 class AbcLocalPartnerWp_Updater {
+
+	/**
+	 * The plugin's file path.
+	 *
+	 * @var string
+	 */
 	private $file;
+
+	/**
+	 * The plugin's metadata.
+	 *
+	 * Parsed from the starting comment in the plugin file.
+	 *
+	 * @var string[]
+	 */
 	private $plugin;
+
+	/**
+	 * The plugin's name.
+	 *
+	 * Parsed from the plugin file path.
+	 *
+	 * @var string
+	 */
 	private $basename;
+
+	/**
+	 * Flag that indicates if the plugin is currently active.
+	 *
+	 * @var bool
+	 */
 	private $active;
+
+	/**
+	 * The GitHub username.
+	 *
+	 * @var string
+	 */
 	private $username;
+
+	/**
+	 * The GitHub repository.
+	 *
+	 * @var string
+	 */
 	private $repository;
-	private $authorize_token;
-	private $github_response;
 
-	public function __construct( $file ) {
+	/**
+	 * The latest plugin version retrieved from GitHub.
+	 *
+	 * @var array|null
+	 */
+	private $latest_version;
+
+	/**
+	 * Initiate plugin properties.
+	 */
+	public function __construct( string $file ) {
 		$this->file = $file;
-		add_action( 'admin_init', [ $this, 'set_plugin_properties' ] );
 
-		return $this;
+		// Postpone the final plugin properties until "admin_init" is fired. This ensures that "get_plugin_data" is available.
+		add_action( 'admin_init', [ $this, 'set_plugin_properties' ] );
 	}
 
+	/**
+	 * Finalize plugin properties.
+	 */
 	public function set_plugin_properties() {
 		$this->plugin   = get_plugin_data( $this->file );
 		$this->basename = plugin_basename( $this->file );
 		$this->active   = is_plugin_active( $this->basename );
 	}
 
-	public function set_username( $username ) {
+	/**
+	 * Set the GitHub username.
+	 */
+	public function set_username( string $username ) {
 		$this->username = $username;
 	}
 
-	public function set_repository( $repository ) {
+	/**
+	 * Set the GitHub repository.
+	 */
+	public function set_repository( string $repository ) {
 		$this->repository = $repository;
 	}
 
-	public function authorize( $token ) {
-		$this->authorize_token = $token;
-	}
-
-	/*
-	 * Get the repository info of the repo
+	/**
+	 * Fetch the latest plugin version from GitHub.
 	 */
-	private function get_repository_info() {
-		if ( is_null( $this->github_response ) ) {
-			$request_uri = sprintf( 'https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository );
+	private function get_latest_plugin_version() {
+		if ( is_null( $this->latest_version ) ) {
+			$request_uri = sprintf(
+				'https://api.github.com/repos/%s/%s/releases',
+				$this->username,
+				$this->repository
+			);
 
-			// Switch to HTTP Basic Authentication for GitHub API v3
-			$curl = curl_init();
+			// Fetch the plugin releases from GitHub.
+			$response = json_decode(
+				wp_remote_retrieve_body( wp_remote_get( $request_uri ) ),
+				true
+			);
 
-			curl_setopt_array( $curl, [
-				CURLOPT_URL            => $request_uri,
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_ENCODING       => "",
-				CURLOPT_MAXREDIRS      => 10,
-				CURLOPT_TIMEOUT        => 0,
-				CURLOPT_FOLLOWLOCATION => true,
-				CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-				CURLOPT_CUSTOMREQUEST  => "GET",
-				CURLOPT_HTTPHEADER     => [
-					"User-Agent: AbcLocalPartnerWp_Updater/1.2.3"
-				]
-			] );
-
-			$response = curl_exec( $curl );
-
-			curl_close( $curl );
-
-			$response = json_decode( $response, true );
-
-			if ( is_array( $response ) ) {
-				$response = current( $response );
+			// If we have a valid response.
+			if( is_array( $response ) ) {
+				// Store the latest release.
+				$this->latest_version = current( $response );
 			}
-
-			if ( $this->authorize_token ) {
-				$response['zipball_url'] = add_query_arg( 'access_token', $this->authorize_token, $response['zipball_url'] );
-			}
-
-			$this->github_response = $response;
 		}
 	}
 
-	/*
-	 * Init
+	/**
+	 * Initialize the plugin updater.
 	 */
 	public function initialize() {
-		add_filter( 'pre_set_site_transient_update_plugins', [ $this, 'modify_transient' ], 10, 1 );
-		add_filter( 'plugins_api', [ $this, 'plugin_popup' ], 10, 3 );
-		add_filter( 'upgrader_post_install', [ $this, 'after_install' ], 10, 3 );
+		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'modify_transient' ), 10, 1 );
+		add_filter( 'plugins_api', array( $this, 'plugin_popup' ), 10, 3 );
+		add_filter( 'upgrader_post_install', array( $this, 'after_install' ), 10, 3 );
 	}
 
-	/*
-	 * Initialise Wordpress method for version control on plugin.
+	/**
+	 * Modify a transient with our plugin information.
+	 *
+	 * @param object $transient
+	 *
+	 * @return mixed
 	 */
 	public function modify_transient( $transient ) {
-		if ( property_exists( $transient, 'checked' ) ) {
-			if ( $checked = $transient->checked ) {
-				$this->get_repository_info();
+		// Did WordPress check for updates?
+		if (property_exists($transient, 'checked') && $checked = $transient->checked) {
+			// Fetch the latest plugin version.
+			$this->get_latest_plugin_version();
 
-				$out_of_date = version_compare( (string)$this->github_response['tag_name'], $checked[ $this->basename ], 'gt' );
+			// Compare with our current version.
+			$out_of_date = version_compare(
+				(string) $this->latest_version['tag_name'],
+				$checked[$this->basename ], 'gt'
+			);
 
-				if ( $out_of_date ) {
-					$new_files = $this->github_response['zipball_url'];
-					$slug      = current( explode( '/', $this->basename ) );
+			if ( $out_of_date ) {
+				// Our plugin is out of date, setup our plugin information.
+				$new_files	= $this->latest_version['zipball_url'];
+				$slug		= current( explode( '/', $this->basename ) );
 
-					$plugin = [
-						'url'         => $this->plugin['PluginURI'],
-						'slug'        => $slug,
-						'package'     => $new_files,
-						'new_version' => (string)$this->github_response['tag_name']
-					];
+				$plugin = [
+					'url'			=> $this->plugin['PluginURI'],
+					'slug'			=> $slug,
+					'package'		=> $new_files,
+					'new_version'	=> (string) $this->latest_version['tag_name']
+				];
 
-					$transient->response[ $this->basename ] = (object) $plugin;
-				}
+				// Modify the transient with our updated plugin information.
+				$transient->response[ $this->basename ] = (object) $plugin;
 			}
 		}
 
 		return $transient;
 	}
 
-	/*
-	 * Addon for "There's a new update"
+	/**
+	 * Provide WordPress plugin popup with infromation about our plugin.
+	 *
+	 * @param mixed $result
+	 * @param mixed $args
+	 *
+	 * @return false|mixed|object
 	 */
-	public function plugin_popup( $result, $action, $args ) {
+	public function plugin_popup( $result, string $action, $args ) {
 		if ( $action !== 'plugin_information' ) {
 			return false;
 		}
 
-		if ( ! empty( $args->slug ) ) {
-			if ( $args->slug == current( explode( '/', $this->basename ) ) ) {
-				$this->get_repository_info();
+		// Check if the slug matches our plugin.
+		if (!empty($args->slug) && $args->slug === current(explode('/', $this->basename))) {
+			$this->get_latest_plugin_version();
 
-				$plugin = [
-					'name'              => $this->plugin['Name'],
-					'slug'              => $this->basename,
-					'requires'          => '5.3',
-					'tested'            => '5.4',
-					'version'           => $this->github_response['tag_name'],
-					'author'            => $this->plugin['AuthorName'],
-					'author_profile'    => $this->plugin['AuthorURI'],
-					'last_updated'      => $this->github_response['published_at'],
-					'homepage'          => $this->plugin['PluginURI'],
-					'short_description' => $this->plugin['Description'],
-					'sections'          => [
-						'Description' => $this->plugin['Description'],
-						'Updates'     => $this->github_response['body'],
-					],
-					'download_link'     => $this->github_response['zipball_url']
-				];
+			$plugin = [
+				'name'				=> $this->plugin['Name'],
+				'slug'				=> $this->basename,
+				'requires'			=> '5.3',
+				'tested'			=> '5.4',
+				'version'			=> $this->latest_version['tag_name'],
+				'author'			=> $this->plugin['AuthorName'],
+				'author_profile'	=> $this->plugin['AuthorURI'],
+				'last_updated'		=> $this->latest_version['published_at'],
+				'homepage'			=> $this->plugin['PluginURI'],
+				'short_description'	=> $this->plugin['Description'],
+				'sections'			=> [
+					'Description'	=> $this->plugin['Description'],
+					'Updates'		=> $this->latest_version['body'],
+				],
+				'download_link'		=> $this->latest_version['zipball_url']
+			];
 
-				return (object) $plugin;
-			}
+			// Return our plugin formation.
+			return (object) $plugin;
 		}
 
+		// Otherwise, return the default information.
 		return $result;
 	}
 
-	/*
-	 * Check if update was successful
+	/**
+	 * Move and reactive our plugin after installation.
+	 *
+	 * @param bool $response
+	 * @param array $hook_extra
+	 * @param array $result
+	 *
+	 * @return array
 	 */
 	public function after_install( $response, $hook_extra, $result ) {
 		global $wp_filesystem;
 
+		// Move our updated plugin files to the plugin directory.
 		$install_directory = plugin_dir_path( $this->file );
 		$wp_filesystem->move( $result['destination'], $install_directory );
 		$result['destination'] = $install_directory;
 
+		// If our plugin was active.
 		if ( $this->active ) {
+			// Reactive it.
 			activate_plugin( $this->basename );
 		}
 
